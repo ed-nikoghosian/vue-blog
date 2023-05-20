bb7b3181f783abe9ef1d1137b84e2d1317f84e01
ed-nikoghosian <ed.nikoghosian@gmail.com>
2023-05-20 14:10:17 +0400

    removed commetns

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-// Add this before server.use(router)
+
 server.use(
   jsonServer.rewriter({
     '/api/*': '/$1',
@@ -40,5 +40,4 @@ server.listen(port, () => {
   console.log('JSON Server is running on', port);
 });
 
-// Export the Server API
 module.exports = server;
